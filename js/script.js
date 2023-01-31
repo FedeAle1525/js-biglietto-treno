7050a6d0910291f5e809cdae98731a759494c296
FedeAle1525 <fedeale.1525@gmail.com>
2023-01-31 14:34:59 +0100

    FIRST COMMIT

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// 1. Chiedere KM a Utente
+// 2. Chiedere ETA' a Utente
+// 3. Calcolare PREZZO BASE del Biglietto
+// 4. Calcolare PREZZO SCONTATO in base a Età
+// 5. Stampare PREZZO su File Html
